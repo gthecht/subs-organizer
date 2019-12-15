c3514c2895bbe153ce2607cd9d1cd4b915f6b94f
giladh <gdpstd@gmail.com>
2019-12-15 20:30:44 +0200

    new build

---
+++ build/precache-manifest.79953c865a915d75d80ee17f9d28e17c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789d3b8aa1e29855d44e7c1a4ef88db4",
+    "url": "/gthecht/subs-organizer/index.html"
+  },
+  {
+    "revision": "d63334d25a6c0a9682c8",
+    "url": "/gthecht/subs-organizer/static/css/main.e73ff761.chunk.css"
+  },
+  {
+    "revision": "83491cf57a9a14ff37ed",
+    "url": "/gthecht/subs-organizer/static/js/2.a0ef5732.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gthecht/subs-organizer/static/js/2.a0ef5732.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d63334d25a6c0a9682c8",
+    "url": "/gthecht/subs-organizer/static/js/main.02740d3c.chunk.js"
+  },
+  {
+    "revision": "5f6e544bfa58d06eea08",
+    "url": "/gthecht/subs-organizer/static/js/runtime-main.daa7342e.js"
+  }
+]);
